ef06b83c4284740950707cd2db506c23a6f681d9
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-08-10 12:04:07 +0900

    問題1の提出

---
+++ src/main/java/org/example/Question1.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Question1 {
+
+  public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+
+      System.out.print("1番目の数字を入力してください：");
+      double number1 = scanner.nextDouble();
+
+      System.out.print("演算子を入力してください（+, -, *, /）：");
+      String operator = scanner.next();
+
+      System.out.print("2番目の数字を入力してください：");
+      double number2 = scanner.nextDouble();
+
+      double result;
+      boolean validCalculation = true;
+
+      switch (operator) {
+        case "+" -> result = number1 + number2;
+        case "-" -> result = number1 - number2;
+        case "*" -> result = number1 * number2;
+        case "/" -> {
+          if (number2 == 0) {
+            System.out.println("エラー：0で割ることはできません");
+            validCalculation = false;
+            result = 0; //resultにダミー値を入力
+
+          } else {
+            result = number1 / number2;
+          }
+
+        }
+
+        default -> {
+          System.out.println(
+              "エラー：演算子が入力されなかったため、計算できません。入力値: " + operator);
+          validCalculation = false;
+          result = 0; //resultにダミー値を入力
+        }
+      }
+
+      if (validCalculation) {
+        System.out.println(number1 + " " + operator + " " + number2 + " " + "= " + result);
+      }
+
+    } catch (InputMismatchException ex) {
+      System.out.println("エラー：数値が入力されなかったため、計算できませんでした");
+    }
+
+  }
+
+}
